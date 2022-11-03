5e2fbf5835bfa50fa6f77150a302275e27a2c31a
Guilherme Vanderley <guivdouradog@gmail.com>
2022-11-03 18:45:21 -0300

    try styles sheet and use touchable opacity

--- App.js
+++ App.js
@@ -1,20 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 
-export default function App() {
-  return (
+export default function App(){
+  const [name, setName] = useState('Guilherme');
+
+  function handleChangeName(){
+    setName('Vanderley');
+  }
+  
+  return(
     <View style={styles.container}>
-      <Text>Hello world!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>{name}</Text>
+
+      <TouchableOpacity style={styles.button} onPress={handleChangeName}>
+        <Text style={styles.buttonText}>Mudar nome</Text>
+      </TouchableOpacity>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#f79ded',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#1313',
+    paddingTop: 28
   },
-});
+  title: {
+    fontSize: 32,
+    color: '#121212',
+    fontWeight: 'bold',
+    textAlign: 'center'
+  },
+  button: {
+    backgroundColor: 'blue',
+    height: 40,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 30
+  },
+  buttonText: {
+    fontSize: 18,
+    color: '#fff',
+    fontWeight: 'bold'
+  }
+})
